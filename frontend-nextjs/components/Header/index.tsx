60c5a7d0cdfad0c848848b93be77bcabd6ee20a2
Gustavo Inacio <incio.gusta@gmail.com>
2022-02-16 20:20:13 -0300

    add footer

--- frontend-nextjs/components/Header/index.tsx
+++ frontend-nextjs/components/Header/index.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export function Header(): JSX.Element | null {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-secondary">
       <div className="container">
         <a className="navbar-brand" href="#">
           GamePools
@@ -20,10 +20,20 @@ export function Header(): JSX.Element | null {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0" />
-          <button className="btn btn-primary me-3">Perguntar</button>
+          <ul className="navbar-nav">
+            <li className="nav-item mb-2 d-flex flex-column">
+              <button className="btn btn-dark me-3">Perguntar</button>
+            </li>
+            <li className="nav-item mb-2 d-flex flex-column">
+              <button className="btn btn-dark me-3 d-block d-lg-none">
+                Login
+              </button>
+            </li>
+          </ul>
+
           <Image
             src="/user-placeholder.png"
-            className="rounded-circle"
+            className="nav-item rounded-circle d-none d-lg-block"
             alt="..."
             height={50}
             width={50}
